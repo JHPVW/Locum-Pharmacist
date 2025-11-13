fe833f4f6944203963409710c6a9abf37849a02f
Jordan Hōani Palmer van Walraven <jordanvan@Jordans-MBP.lan>
2025-11-13 11:39:55 +1100

    Fix import order using eslint autofix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,8 @@ import 'tailwindcss/tailwind.css';
 import '../globalStyles.scss';
 
 import {SpeedInsights} from '@vercel/speed-insights/next';
-import {memo} from 'react';
-
 import type {AppProps} from 'next/app';
+import {memo} from 'react';
 
 const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
   return (
