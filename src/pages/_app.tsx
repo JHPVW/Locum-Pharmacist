6a6c5ecca2398415fe6ecac2c32aa391523262ea
Jordan Hōani Palmer van Walraven <jordanvan@Jordans-MBP.lan>
2025-11-13 11:33:17 +1100

    Add Vercel Speed Insights for performance monitoring

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import 'tailwindcss/tailwind.css';
 import '../globalStyles.scss';
 
 import type {AppProps} from 'next/app';
+import {SpeedInsights} from '@vercel/speed-insights/next';
 import {memo} from 'react';
 
 const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
   return (
     <>
       <Component {...pageProps} />
+      <SpeedInsights />
     </>
   );
 });
