a29ca16fedbc4ccea96eb27bd15bf33b2e1a0dfd
Jordan Hōani Palmer van Walraven <jordanvan@Jordans-MBP.lan>
2025-11-13 11:34:52 +1100

    Fix import order in _app.tsx for linting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
-import 'tailwindcss/tailwind.css';
-import '../globalStyles.scss';
-
 import type {AppProps} from 'next/app';
 import {SpeedInsights} from '@vercel/speed-insights/next';
 import {memo} from 'react';
+
+import 'tailwindcss/tailwind.css';
+import '../globalStyles.scss';
 
 const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
   return (
