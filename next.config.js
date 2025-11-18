00fbccf2ca3532235f0d797fb67b299c297e0dc2
Jordan Hōani Palmer van Walraven <jordanvan@Jordans-MacBook-Pro.local>
2025-11-18 14:13:02 +1100

    Add Google Ads conversion tracking (gtag.js)

--- next.config.js
+++ next.config.js
@@ -67,11 +67,11 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com",
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' data: https:",
               "font-src 'self' data:",
-              "connect-src 'self'",
+              "connect-src 'self' https://www.googletagmanager.com https://www.google-analytics.com",
               "frame-ancestors 'none'",
             ].join('; '),
           },
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,25 @@ import '../globalStyles.scss';
 
 import {SpeedInsights} from '@vercel/speed-insights/next';
 import type {AppProps} from 'next/app';
+import Script from 'next/script';
 import {memo} from 'react';
 
 const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
   return (
     <>
+      {/* Google Ads */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17736898219"
+        strategy="afterInteractive"
+      />
+      <Script id="google-ads" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-17736898219');
+        `}
+      </Script>
       <Component {...pageProps} />
       <SpeedInsights />
     </>
